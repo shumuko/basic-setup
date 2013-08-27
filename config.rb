abbacfe7822ceb85666f6f976966717f5e9c5d42
Fion Kou <fionukou@gmail.com>
2013-08-27 11:22:02 -0400

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
-# Require any additional compass plugins here.
+# Compass config.rb
+
+preferred_syntax = :scss
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -6,6 +8,9 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+
+line_comments = (environment == :production) ? false : true
+output_style  = (environemnt == :production) ? :compressed : :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
